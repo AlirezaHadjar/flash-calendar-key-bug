ca6ccefdc5b44ca140c14a5cf780b75139f6efbb
Alireza Hadjar <asiahadjar@gmail.com>
2024-04-26 13:23:23 +0330

    reproduced the warning

--- App.tsx
+++ App.tsx
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  CalendarActiveDateRange,
+  CalendarOnDayPress,
+  fromDateId,
+  toDateId,
+} from "@marceloterreiro/flash-calendar";
+import dayjs from "dayjs";
+import { useCallback, useMemo, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MCalendar } from "./Calendar";
 
 export default function App() {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(
+    new Date()
+  );
+  const [currentCalendarMonth, setCurrentCalendarMonth] = useState(new Date());
+
+  const handleDayPress = useCallback<CalendarOnDayPress>((dateId) => {
+    setCurrentCalendarMonth(fromDateId(dateId));
+    setSelectedDate(fromDateId(dateId));
+  }, []);
+
+  const calendarActiveDateRanges = useMemo<CalendarActiveDateRange[]>(() => {
+    if (!selectedDate) {
+      return [];
+    }
+    return [
+      {
+        startId: toDateId(selectedDate),
+        endId: toDateId(selectedDate),
+      },
+    ];
+  }, [selectedDate]);
+
+  const handlePreviousMonth = useCallback(() => {
+    const newDate = dayjs(currentCalendarMonth).subtract(1, "month").toDate();
+
+    setCurrentCalendarMonth(newDate);
+  }, [currentCalendarMonth]);
+
+  const handleNextMonth = useCallback(() => {
+    const newDate = dayjs(currentCalendarMonth).add(1, "month").toDate();
+
+    setCurrentCalendarMonth(newDate);
+  }, [currentCalendarMonth]);
+
+  const handleMonthYearChange = useCallback(
+    (month: number, year: number) => {
+      let newDate = dayjs(currentCalendarMonth)
+        .set("month", month)
+        .set("year", year)
+        .toDate();
+      setCurrentCalendarMonth(newDate);
+    },
+    [currentCalendarMonth]
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MCalendar
+        calendarActiveDateRanges={calendarActiveDateRanges}
+        calendarMonthId={toDateId(currentCalendarMonth)}
+        currentCalendarMonth={currentCalendarMonth}
+        onCalendarDayPress={handleDayPress}
+        onNextMonthPress={handleNextMonth}
+        onMonthYearChange={handleMonthYearChange}
+        onPreviousMonthPress={handlePreviousMonth}
+      />
     </View>
   );
 }
@@ -13,8 +73,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 200,
+    paddingHorizontal: 20,
   },
 });
---
+++ Calendar.tsx
@@ -0,0 +1,104 @@
+import type {
+  CalendarProps,
+  CalendarTheme,
+} from "@marceloterreiro/flash-calendar";
+import { Calendar, useCalendar } from "@marceloterreiro/flash-calendar";
+import React, { memo, useMemo } from "react";
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
+
+import dayjs from "dayjs";
+
+const DAY_HEIGHT = 40;
+const WEEK_DAYS_HEIGHT = 36;
+
+interface MCalendarProps extends CalendarProps {
+  onPreviousMonthPress: () => void;
+  onNextMonthPress: () => void;
+  onMonthYearChange: (month: number, year: number) => void;
+  containerStyle?: StyleProp<ViewStyle>;
+  currentCalendarMonth: Date;
+}
+export const MCalendar = memo((props: MCalendarProps) => {
+  const { calendarRowMonth, weekDaysList, weeksList } = useCalendar({
+    getCalendarWeekDayFormat: (date) => dayjs(date).format("ddd").toUpperCase(),
+    ...props,
+  });
+
+  return (
+    <View style={props.containerStyle}>
+      <Calendar.VStack spacing={props.calendarRowVerticalSpacing}>
+        <Calendar.HStack
+          alignItems="center"
+          justifyContent="space-between"
+          width="100%"
+        >
+          <TouchableOpacity hitSlop={20} onPress={props.onPreviousMonthPress}>
+            <View style={styles.circle} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              props.onMonthYearChange(
+                dayjs(props.currentCalendarMonth).month(),
+                2020
+              );
+            }}
+          >
+            <Text>{calendarRowMonth}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity hitSlop={20} onPress={props.onNextMonthPress}>
+            <View style={styles.circle} />
+          </TouchableOpacity>
+        </Calendar.HStack>
+
+        <View>
+          <Calendar.Row.Week spacing={4}>
+            {weekDaysList.map((week, i) => (
+              <Calendar.Item.WeekName height={WEEK_DAYS_HEIGHT} key={i}>
+                {week}
+              </Calendar.Item.WeekName>
+            ))}
+          </Calendar.Row.Week>
+
+          <Calendar.VStack spacing={10}>
+            {weeksList.map((week, i) => (
+              <Calendar.Row.Week key={i}>
+                {week.map((day) => (
+                  <Calendar.Item.Day.Container
+                    dayHeight={DAY_HEIGHT}
+                    daySpacing={4}
+                    isStartOfWeek={day.isStartOfWeek}
+                    key={day.id}
+                  >
+                    <Calendar.Item.Day
+                      height={DAY_HEIGHT}
+                      metadata={day}
+                      onPress={props.onCalendarDayPress}
+                    >
+                      {day.displayLabel}
+                    </Calendar.Item.Day>
+                  </Calendar.Item.Day.Container>
+                ))}
+              </Calendar.Row.Week>
+            ))}
+          </Calendar.VStack>
+        </View>
+      </Calendar.VStack>
+    </View>
+  );
+});
+
+const styles = StyleSheet.create({
+  circle: {
+    width: 20,
+    height: 20,
+    backgroundColor: "grey",
+    borderRadius: 10,
+  },
+});
